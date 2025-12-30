56ebc2b4e9f313ed0c94c8dfd6ce3630e34b688d
m3rr <143431072+m3rr@users.noreply.github.com>
2025-12-30 07:24:31 -0500

    Fixed Versioning

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 # h4_Live Version Control
 # ------------------------------------------------------------------------------
 
-__version__ = "1.0.3"
+__version__ = "2.0.0"
 __author__ = "(h4)"
 __status__ = "Alpha - Nuclear Option - Scortched Eart - No Mercy - No Forgiveness"
