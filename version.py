2d796ea65f88360639022b1266b27586e6ef8627
m3rr <143431072+m3rr@users.noreply.github.com>
2026-02-08 00:37:38 -0500

    versioning

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 # h4_Live Version Control
 # ------------------------------------------------------------------------------
 
-__version__ = "4.6.0"
+__version__ = "4.6.5"
 __author__ = "(h4)"
 __status__ = "Beta - Nuclear Logic - Persistent State"
