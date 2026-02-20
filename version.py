e53efdfab45a85320bdd4fb86797d58320f03f58
m3rr <143431072+m3rr@users.noreply.github.com>
2026-02-20 16:53:06 -0500

    fucking versioning

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 # h4_Live Version Control
 # ------------------------------------------------------------------------------
 
-__version__ = "5.7.5"
+__version__ = "5.7.6"
 __author__ = "(h4)"
 __status__ = "Beta - Nuclear Logic - Persistent State"
