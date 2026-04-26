9e11398667fe6f9fc5aee2fc6c87a44271211903
_h4 <143431072+m3rr@users.noreply.github.com>
2026-04-26 06:46:34 -0400

    Found more issues - smart save previews were taking forever, so updated their method to use a manifest properly

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 # h4_Live Version Control
 # ------------------------------------------------------------------------------
 
-__version__ = "9.5.4"
+__version__ = "9.5.5"
 __author__ = "(h4)"
 __status__ = "Beta - Nuclear Logic - Persistent State"
