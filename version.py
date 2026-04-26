a5809c4c724b16bc466fbfac08c48f094baf48a7
_h4 <143431072+m3rr@users.noreply.github.com>
2026-04-26 06:37:17 -0400

    Ok - found that the last pushed had major bugs. Fixed them - updated the save node, also introducing the Pythoninpulator-inator! Modify and alter images with VFX , and otherwise just mess with the image. See read me updates and book of h4 for more details. + Fixed layering issue

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 # h4_Live Version Control
 # ------------------------------------------------------------------------------
 
-__version__ = "9.5.3"
+__version__ = "9.5.4"
 __author__ = "(h4)"
 __status__ = "Beta - Nuclear Logic - Persistent State"
