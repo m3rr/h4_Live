87986ea1fd3e7e042a0214def8ce732da7d026f7
_h4 <143431072+m3rr@users.noreply.github.com>
2026-04-27 11:53:41 -0400

    Fixed some loading issues with smart save I never noticed and made it more of a soverign node. It has and is now the source for the panel system in the dashboard, it shares it's build with it and is the first modular designed node I have - That can be seperated and modified. Not yet - but the scaffolding is there

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 # h4_Live Version Control
 # ------------------------------------------------------------------------------
 
-__version__ = "10.1.2"
+__version__ = "10.5.0"
 __author__ = "(h4)"
 __status__ = "Production Hardened - Forensic Core"
