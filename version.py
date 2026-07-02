c4c3e87756b1e73493b5943e6ce5613618d3b24d
_h4 <143431072+m3rr@users.noreply.github.com>
2026-07-02 11:31:04 -0400

    Bump version to 11.0.2

--- version.py
+++ version.py
@@ -3,6 +3,6 @@
 # h4_Live Version Control
 # ------------------------------------------------------------------------------
 
-__version__ = "11.0.1"
+__version__ = "11.0.2"
 __author__ = "(h4)"
 __status__ = "Production Hardened - Forensic Core"
